d34be8f37434ffed244b73b8b6e7da0784075a3e
Zhibra <108349070+Zhibra@users.noreply.github.com>
2022-06-29 12:32:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
+global.owner = ['6289532142617','6285156784562','6288292024190']
 global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.packname = 'toyye'
+global.author = 'botwa'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
